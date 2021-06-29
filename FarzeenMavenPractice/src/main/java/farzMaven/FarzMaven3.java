edb2bf1973ca69a571dedb12cc1b32bac373cf4b
SaimaAlam <Saimasalam882@yahoo.com>
2021-06-29 09:26:54 -0400

    hi first commit

--- FarzeenMavenPractice/src/main/java/farzMaven/FarzMaven3.java
+++ FarzeenMavenPractice/src/main/java/farzMaven/FarzMaven3.java
@@ -3,8 +3,7 @@ package farzMaven;
 public class FarzMaven3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("Hi Farzee How are you?");
 	}
 
 }
