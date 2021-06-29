a7543a823ee7c2a43a3547326b32399de62aded0
shahin <shahinurakter085bd@gmail.com>
2021-06-29 10:21:49 -0400

    I add even numbers.

--- FarzeenMavenPractice/src/main/java/farzMaven/FarzMaven3.java
+++ FarzeenMavenPractice/src/main/java/farzMaven/FarzMaven3.java
@@ -3,7 +3,19 @@ package farzMaven;
 public class FarzMaven3 {
 
 	public static void main(String[] args) {
-System.out.println("Hi Farzee How are you?");
+System.out.println("Hi Farzee How are you?"); 
+ int a =100;
+
+for(int i=0; i<=100;i++) {
+	System.out.println("even number:"+i);
+	i++;
+	
+} 
+System.out.println("Hi Saima and Farzeen ,I add some code .");
+System.out.println("Hi this is shahin");
+
+
+
 	}
 
 }
